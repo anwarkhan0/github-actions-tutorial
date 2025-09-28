612d5e36875c717fb5f1dcb8493b37cd9dca8933
anwarkhan0 <anwarsyed01234@gmail.com>
2025-09-28 22:57:09 +0300

    update 39

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "woo hoo! CI/CD with GitHub Actions is working!" });
+  res.json({ message: "CI/CD with GitHub Actions is working!" });
 });
 
 module.exports = app;
